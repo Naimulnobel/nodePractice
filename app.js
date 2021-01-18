3727f6e64cc8d2c0932be7558f869f97bc8121d9
namulnobel <nobelnaimulislam@gmail.com>
2021-01-18 18:16:30 +0600

    node practice

--- app.js
+++ app.js
@@ -22,11 +22,60 @@
 // const fs =require('fs')
 // fs.writeFileSync('text.txt','yo yo')
 // fs.appendFileSync('text.txt','haha')
-// const yourNote=require('./note.js')
+const note=require('./note.js')
 // // console.log(yourNote)
 // const note=yourNote()
 // console.log(note)
 const chalk = require('chalk');
-console.log(chalk.green("Success"))
+const yargs = require('yargs');
+
+// 
+yargs.command({
+    command:'add',
+    describe:'Adding a note',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Note Body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        note.addNote(argv.title, argv.body)
+        
+    }
+
+})
+yargs.command({
+    command:'remove',
+    describe:'Removing a note',
+    handler: function(){
+        console.log('Removing a note')
+    }
+
+})
+yargs.command({
+    command:'read',
+    describe:'Reading a list',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Reading a list',argv)
+    }
+
+})
+//console.log(yargs.argv)
+yargs.parse()
+    
 
 
